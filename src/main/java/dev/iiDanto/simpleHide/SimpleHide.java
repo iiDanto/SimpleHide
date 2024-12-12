1e84416c975325dd1649826773f24268b3922adc
iiDanto <158697195+iiDanto@users.noreply.github.com>
2024-12-12 11:08:55 +0100

    Initial Commit

---
+++ src/main/java/dev/iiDanto/simpleHide/SimpleHide.java
@@ -0,0 +1,17 @@
+package dev.iiDanto.simpleHide;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleHide extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
